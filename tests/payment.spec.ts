549ddf4d86896dd7d019925947e46bf165e7856f
b1izz2143 <b1izz2143@gmail.com>
2024-11-14 16:45:23 +0100

    Inital commit

--- tests/payment.spec.ts
+++ tests/payment.spec.ts
@@ -18,8 +18,7 @@ test.describe('Payment tests', () => {
     await loginPage.loginInput.fill(userID);
     await loginPage.passwordInput.fill(userPassword);
     await loginPage.loginButton.click();
-
-    
+    //test
     // await page.getByTestId('login-input').fill(userID);
     // await page.getByTestId('password-input').fill(userPassword);
     // await page.getByTestId('login-button').click();
