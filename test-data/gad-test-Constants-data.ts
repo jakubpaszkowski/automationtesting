5144faf9916c201838a99d5be94515f09a7a27a9
b1izz2143 <b1izz2143@gmail.com>
2024-11-17 20:20:47 +0100

    refactor + centralising test data to /test-data/qad-test-Constants-data - as new object has been created - and optimising the handling of checkboxes and assertions

---
+++ test-data/gad-test-Constants-data.ts
@@ -0,0 +1,17 @@
+export const testConstants = {
+    textToInput: "test",
+    inputValueText: "Input value changed to: test",
+    textareaInputValue: "Textarea value changed to: test",
+    dropdownOption2: "option2",
+    textRadioButton2: "Radio Button 2 clicked!",
+    textRangeValue: "Range value changed to: 52",
+    rangeValue: "52",
+    dateToFill: "2024-11-16",
+    selectedDate: "Selected date: 2024-11-16",
+    colorToFill: "#00fbff",
+    colorResults: "New selected color: #00fbff as hex and in RGB: rgb(0, 251, 255)",
+    buttonClickedResultText: "You clicked the button!",
+    checkboxClickedResultText: "Checkbox is checked!",
+
+
+};
--- tests/gad-tests.spec.ts
+++ tests/gad-tests.spec.ts
@@ -1,194 +1,264 @@
-import { test, expect } from '@playwright/test';
-import { SimpleElements, AccountPage, SimpleReservations, DisabledElements } from '../pages/gad.page';
+import { test, expect } from "@playwright/test";
+import { testConstants } from "../test-data/gad-test-Constants-data"; //for gad tests
+import {
+  SimpleElements,
+  AccountPage,
+  SimpleReservations,
+  DisabledElements,
+} from "../pages/gad.page";
 
 test.describe("Locator filters", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:3000/practice/simple-elements.html');
+    await page.goto("http://localhost:3000/practice/simple-elements.html");
   });
-
 
   test.describe("Finding element - different approaches", () => {
     test("Single button click using options", async ({ page }) => {
-      
-        const elementsPage = new SimpleElements(page);
-    await elementsPage.buttonId.click();
-    await expect.soft(elementsPage.resultsId).toHaveText("You clicked the button!");
-    await elementsPage.checkboxId.click();
-    
-   
-        
-    await expect.soft(elementsPage.resultsId).toHaveText("Checkbox is checked!");
-    await elementsPage.dropDownNoId1.selectOption('option2');
-    await expect.soft(elementsPage.resultsId).toHaveText("Selected option: option2");
-    await elementsPage.inputId.fill("testing");
-    await elementsPage.inputId.blur();
-    await expect.soft(elementsPage.resultsId).toHaveText("Input value changed to: testing");
-    await elementsPage.radioButtons1Id.click();
-    await expect.soft(elementsPage.resultsId).toHaveText("Radio Button 1 clicked!");
-    await page.getByRole('radio').nth(1).click();
-    await expect.soft(elementsPage.resultsId).toHaveText("Radio Button 2 clicked!");
-    await elementsPage.rangeId.fill("50");
-    await expect.soft(elementsPage.resultsId).toHaveText("Range value changed to: 50");
-    await elementsPage.colorPaletteId.fill("#02f2d6");
-    await expect.soft(elementsPage.resultsId).toHaveText("New selected color: #02f2d6 as hex and in RGB: rgb(2, 242, 214)");
+      const elementsPage = new SimpleElements(page);
+      await elementsPage.buttonId.click();
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("You clicked the button!");
+      await elementsPage.checkboxId.click();
 
-
-    
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("Checkbox is checked!");
+      await elementsPage.dropDownNoId1.selectOption("option2");
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("Selected option: option2");
+      await elementsPage.inputId.fill("testing");
+      await elementsPage.inputId.blur();
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("Input value changed to: testing");
+      await elementsPage.radioButtons1Id.click();
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("Radio Button 1 clicked!");
+      await page.getByRole("radio").nth(1).click();
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("Radio Button 2 clicked!");
+      await elementsPage.rangeId.fill("50");
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText("Range value changed to: 50");
+      await elementsPage.colorPaletteId.fill("#02f2d6");
+      await expect
+        .soft(elementsPage.resultsId)
+        .toHaveText(
+          "New selected color: #02f2d6 as hex and in RGB: rgb(2, 242, 214)"
+        );
     });
 
-
     test("Single button click (using filter and hasText)", async ({ page }) => {
-      
       // Arrange:
       const elementRole = "button";
       const resultId = "dti-results";
       const expectedMessage = "You clicked the button!";
       const elementText = "Click me!";
 
-      const buttonLocator = page.getByRole(elementRole).filter({hasText: elementText});
-        const resultsLocator = page.getByTestId(resultId);
-      
-      // Act:
-        await buttonLocator.click();
+      const buttonLocator = page
+        .getByRole(elementRole)
+        .filter({ hasText: elementText });
+      const resultsLocator = page.getByTestId(resultId);
 
+      // Act:
+      await buttonLocator.click();
 
       // Assert:
       await expect(resultsLocator).toHaveText(expectedMessage);
-
-
     });
-    
+
     test("Simple redervation", async ({ page }) => {
-        await page.goto('http://localhost:3000/practice/simple-reservation-v1.html');
+      await page.goto(
+        "http://localhost:3000/practice/simple-reservation-v1.html"
+      );
 
-        const elementsPage = new SimpleReservations(page);
-        await elementsPage.food.click();
-        await elementsPage.date23102024.click();
-        await elementsPage.checkoutButton.click();
-        await expect.soft(elementsPage.results).toHaveText("Reservation for 23.10.2024 with features: Food for total price: 150$");
-  
-      });
+      const elementsPage = new SimpleReservations(page);
+      await elementsPage.food.click();
+      await elementsPage.date23102024.click();
+      await elementsPage.checkoutButton.click();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(
+          "Reservation for 23.10.2024 with features: Food for total price: 150$"
+        );
+    });
 
-      test("Elements with changing state", async ({ page }) => {
-        await page.goto('http://localhost:3000/practice/disabled-elements-1.html');
-        const textToInput = "test";
-        const inputValueText = "Input value changed to: test";
-        const textareaInputValue = "Textarea value changed to: test";
-        const dropdownOption2 = 'option2';
-        const textRadioButton2 = "Radio Button 2 clicked!";
-        const textRangeValue = "Range value changed to: 52";
-        const rangeValue = "52";
-        const dateToFill = "2024-11-16"
-        const selectedDate = "Selected date: 2024-11-16"
-        const colorToFill = "#00fbff";
-        const colorResults = "New selected color: #00fbff as hex and in RGB: rgb(0, 251, 255)";
-        const elementsPage = new DisabledElements(page);
+    test("Elements with changing state", async ({ page }) => {
+      await page.goto(
+        "http://localhost:3000/practice/disabled-elements-1.html"
+      );
+      const elementsPage = new DisabledElements(page);
 
-         // Find span element
-  const delayLabel = page.locator('#delayLabel');
+      // Find span element
+      const delayLabel = page.locator("#delayLabel");
 
-  // Wait until element is visible
-  await delayLabel.waitFor({ state: 'visible' });
+      // Wait until element is visible
+      await delayLabel.waitFor({ state: "visible" });
 
-  // Take element's text
-  const delayText = await delayLabel.textContent();
+      // Take element's text
+      const delayText = await delayLabel.textContent();
 
-  // Convert text to number (seconds)
-  const delayInSeconds = parseFloat(delayText || '0');
+      // Convert text to number (seconds)
+      const delayInSeconds = parseFloat(delayText || "0");
 
-  // Dynamic waiting (conversion to milliseconds)
-  if (!isNaN(delayInSeconds)) {
-    await page.waitForTimeout(delayInSeconds * 1000);
-    console.log(`Waited for ${delayInSeconds} seconds`);
-  } else {
-    console.error('Invalid delay value in #delayLabel');
-  }
+      // Dynamic waiting (conversion to milliseconds)
+      if (!isNaN(delayInSeconds)) {
+        await page.waitForTimeout(delayInSeconds * 1000);
+        console.log(`Waited for ${delayInSeconds} seconds`);
+      } else {
+        console.error("Invalid delay value in #delayLabel");
+      }
 
-  // Continue testing after dynamic waiting
-   await expect.soft(page.getByRole('heading', { name: 'Elements are now: ENABLED -' })).toHaveText(/Elements are now: ENABLED - please wait.*/);
-   await elementsPage.buttonId.click();
-   await expect.soft(elementsPage.results).toHaveText("You clicked the button!");
-   await elementsPage.checkbox.click();
-   //multiple assertion types :)
-   await expect.soft(elementsPage.results).toHaveText("Checkbox is checked!");
-   await expect.soft(elementsPage.results).toBeVisible();
-   await expect(elementsPage.checkbox).toBeChecked();
-   // click again to uncheck
-   await elementsPage.checkbox.click();
+      // Continue testing after dynamic waiting
+      await expect
+        .soft(
+          page.getByRole("heading", { name: "Elements are now: ENABLED -" })
+        )
+        .toHaveText(/Elements are now: ENABLED - please wait.*/);
+      await elementsPage.buttonId.click();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText("You clicked the button!");
+      await elementsPage.checkbox.click();
+      //multiple assertion types :)
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText("Checkbox is checked!");
+      await expect.soft(elementsPage.results).toBeVisible();
+      await expect(elementsPage.checkbox).toBeChecked();
+      // click again to uncheck
+      await elementsPage.checkbox.click();
 
-   // verify if checkbox is NOT checked
-   await expect(elementsPage.checkbox).not.toBeChecked();
+      // verify if checkbox is NOT checked
+      await expect(elementsPage.checkbox).not.toBeChecked();
 
-  
+      //assertion
+      await expect.soft(elementsPage.heading).toBeVisible();
+      await elementsPage.input.fill(testConstants.textToInput);
+      await elementsPage.input.blur();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.inputValueText);
+      await elementsPage.textarea.fill(testConstants.textToInput);
+      await elementsPage.textarea.blur();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.textareaInputValue);
+      await elementsPage.dropdown.selectOption(testConstants.dropdownOption2);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(`Selected option: ${testConstants.dropdownOption2}`);
+      await elementsPage.radio.check();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.textRadioButton2);
+      await elementsPage.range.fill(testConstants.rangeValue);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.textRangeValue);
+      await elementsPage.date.fill(testConstants.dateToFill);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.selectedDate);
+      await elementsPage.color.fill(testConstants.colorToFill);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.colorResults);
+    });
 
- //assertion
-  await expect.soft(elementsPage.heading).toBeVisible();
-  await elementsPage.input.fill(textToInput);
-  await elementsPage.input.blur();
-  await expect.soft(elementsPage.results).toHaveText(inputValueText);
-  await elementsPage.textarea.fill(textToInput);
-  await elementsPage.textarea.blur();
-  await expect.soft(elementsPage.results).toHaveText(textareaInputValue);
-  await elementsPage.dropdown.selectOption(dropdownOption2);
-  await expect.soft(elementsPage.results).toHaveText(`Selected option: ${dropdownOption2}`);
-  await elementsPage.radio.check();
-  await expect.soft(elementsPage.results).toHaveText(textRadioButton2);
-  await elementsPage.range.fill(rangeValue);
-  await expect.soft(elementsPage.results).toHaveText(textRangeValue);
-  await elementsPage.date.fill(dateToFill);
-  await expect.soft(elementsPage.results).toHaveText(selectedDate);
-  await elementsPage.color.fill(colorToFill);
-  await expect.soft(elementsPage.results).toHaveText(colorResults);
+    test("button are no displayed for X seconds, hardcoded", async ({
+      page,
+    }) => {
+      await page.goto(
+        "http://localhost:3000/practice/not-displayed-elements-1.html"
+      );
 
+      const elementsPage = new DisabledElements(page);
+      const button = elementsPage.buttonId;
+      await button.waitFor({ state: "visible", timeout: 5000 });
+    });
+
+    test("button are no displayed for X seconds dynamic", async ({ page }) => {
+      await page.goto(
+        "http://localhost:3000/practice/not-displayed-elements-1.html"
+      );
+      const elementsPage = new DisabledElements(page);
+      // const button = elementsPage.buttonId;
+      // await button.waitFor({ state: 'visible', timeout: 5000 });
+
+      // Find span element
+      const delayLabel = page.locator("#delayLabel");
+
+      // Wait until element is visible
+      await delayLabel.waitFor({ state: "visible" });
+
+      // Take element's text
+      const delayText = await delayLabel.textContent();
+
+      // Convert text to number (seconds)
+      const delayInSeconds = parseFloat(delayText || "0");
+
+      // Dynamic waiting (conversion to milliseconds)
+      if (!isNaN(delayInSeconds)) {
+        await page.waitForTimeout(delayInSeconds * 1000);
+        console.log(`Waited for ${delayInSeconds} seconds`);
+      } else {
+        console.error("Invalid delay value in #delayLabel");
+      }
+
+      // Continue testing after dynamic waiting
+      await expect
+        .soft(
+          page.getByRole("heading", { name: "Elements are now: DISPLAYED -" })
+        )
+        .toHaveText(/Elements are now: DISPLAYED - please wait.*/);
+      await elementsPage.buttonId.click();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.buttonClickedResultText);
+      await elementsPage.checkbox.check();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.checkboxClickedResultText);
+      await expect.soft(elementsPage.checkbox).toBeChecked();
+      //different method for checkbox
+      await elementsPage.checkbox.setChecked(false);
+      await expect.soft(elementsPage.checkbox).not.toBeChecked();
+      await elementsPage.input.fill(testConstants.textToInput);
+      await elementsPage.input.blur();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.inputValueText);
+      await elementsPage.textarea.fill(testConstants.textToInput);
+      await elementsPage.textarea.blur();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.textareaInputValue);
+      await elementsPage.dropdown.selectOption(testConstants.dropdownOption2);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(`Selected option: ${testConstants.dropdownOption2}`);
+      await elementsPage.radio.check();
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.textRadioButton2);
+      await elementsPage.range.fill(testConstants.rangeValue);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.textRangeValue);
+      await elementsPage.date.fill(testConstants.dateToFill);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.selectedDate);
+      await elementsPage.color.fill(testConstants.colorToFill);
+      await expect
+        .soft(elementsPage.results)
+        .toHaveText(testConstants.colorResults);
+    });
   });
-
-  test("button are no displayed for X seconds, hardcoded", async ({ page }) => {
-    await page.goto('http://localhost:3000/practice/not-displayed-elements-1.html');
-
-    const elementsPage = new DisabledElements(page);
-    const button = elementsPage.buttonId;
-    await button.waitFor({ state: 'visible', timeout: 5000 });
-
-  });
-
-  test("button are no displayed for X seconds dynamic", async ({ page }) => {
-    await page.goto('http://localhost:3000/practice/not-displayed-elements-1.html');
-
-    const elementsPage = new DisabledElements(page);
-    // const button = elementsPage.buttonId;
-    // await button.waitFor({ state: 'visible', timeout: 5000 });
-
-        // Find span element
-  const delayLabel = page.locator('#delayLabel');
-
-  // Wait until element is visible
-  await delayLabel.waitFor({ state: 'visible' });
-
-  // Take element's text
-  const delayText = await delayLabel.textContent();
-
-  // Convert text to number (seconds)
-  const delayInSeconds = parseFloat(delayText || '0');
-
-  // Dynamic waiting (conversion to milliseconds)
-  if (!isNaN(delayInSeconds)) {
-    await page.waitForTimeout(delayInSeconds * 1000);
-    console.log(`Waited for ${delayInSeconds} seconds`);
-  } else {
-    console.error('Invalid delay value in #delayLabel');
-  }
-
-  // Continue testing after dynamic waiting
-   await expect.soft(page.getByRole('heading', { name: 'Elements are now: DISPLAYED -' })).toHaveText(/Elements are now: DISPLAYED - please wait.*/);
-   await elementsPage.buttonId.click();
-
-
-
-
-
-
-
-  });
-  });
-
 });
